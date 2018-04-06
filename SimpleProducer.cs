e2386f903314104007dc4cfd4c4cedbc7d0f6546
aarikmudgal <aarikmudgal@gmail.com>
2018-04-06 20:40:49 +0530

    Update SimpleProducer.cs

--- SimpleProducer.cs
+++ SimpleProducer.cs
@@ -13,7 +13,8 @@ namespace kafkaProducer
             Console.WriteLine("Inside Produce method");
             var config = new Dictionary<string, object>
             {
-                {"bootstrap.servers", "35.200.241.185:9092" }
+                {"bootstrap.servers", "35.200.241.185:9092" },
+                { "produce.offset.report", true }
             };
 
             Console.WriteLine("Config set");
@@ -21,19 +22,32 @@ namespace kafkaProducer
            {
                using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
                {
-                    Console.WriteLine($"Config {config["bootstrap.servers"].ToString()}");
+                    ////Console.WriteLine($"Config {config["bootstrap.servers"].ToString()}");
                    //producer.ProduceAsync("simpletest", null, message).GetAwaiter().GetResult();
                    //producer.Flush(100);
                    
                    //var dr = producer.ProduceAsync("simpletest", null, message).Result;
-                   var deliveryReport = producer.ProduceAsync("simpletest", null, message);
-                    deliveryReport.ContinueWith(task =>
-                    {
-                        Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}, Error: {task.Result.Error}");
-                    });
-                   Console.WriteLine($"ProduceAsync called...{deliveryReport}");
+                  //// var deliveryReport = producer.ProduceAsync("simpletest", null, message);
+                    ////deliveryReport.ContinueWith(task =>
+                    ////{
+                     ////   Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}, Error: {task.Result.Error}");
+                  //// });
+                   ////Console.WriteLine($"ProduceAsync called...{deliveryReport}");
                    //Console.WriteLine($"Delivered '{dr.Value}' to: {dr.TopicPartitionOffset}");
-                   producer.Flush(TimeSpan.FromSeconds(10));
+                   ////producer.Flush(TimeSpan.FromSeconds(10));
+                    var tasks = new List<Task<Message<string, string>>>();
+
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var result = producer.ProduceAsync("Testing", $"key-{i}", $"Testing #{i}-{DateTime.Now.ToLongTimeString()}");//.Result;
+
+                        result.ContinueWith(res =>
+                        {
+                            Console.WriteLine(
+                                $"Partition: {res.Result.Partition}, Offset: {res.Result.Offset}, {res.Result.Key} {res.Result.Value}");
+                        });
+                    }
+                    producer.Flush(TimeSpan.FromSeconds(10));
                };
            }
            catch (Exception ex)
