4fdab68e5477ad3d1b8b2e9b556ce1d8f40dca4e
aarikmudgal <aarikmudgal@gmail.com>
2018-04-12 14:44:20 +0530

    Update SimpleProducer.cs

--- SimpleProducer.cs
+++ SimpleProducer.cs
@@ -14,7 +14,7 @@ namespace kafkaProducer
             Console.WriteLine("Inside Produce method");
             var config = new Dictionary<string, object>
             {
-                {"bootstrap.servers", "35.200.142.220:9092" },
+                {"bootstrap.servers", "35.201.187.234:9092" },
                 {"debug", "all"},
                 { "produce.offset.report", true }
             };
